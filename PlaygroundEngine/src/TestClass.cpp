8357c55017419f6ee3b23a4d04cd4215701c70fd
Cristofer Oswald <cristoferoswald@gmail.com>
2026-05-31 00:00:18 -0400

    Added global UTF8 compile option for Windows to avoid module incompatibilities.

--- PlaygroundEngine/src/TestClass.cpp
+++ PlaygroundEngine/src/TestClass.cpp
@@ -1,9 +1,15 @@
+module;
+
+#include "PlaygroundEngine/Log.h"
+
 module Test;
 
 import std;
 
 int PlaygroundEngine::Test::GetA()
 {
+    LOG_TRACE("test");
+    
     std::cout << "test" << "\n";
     return a;
 }
